b4a0a215b831813afca8af017e5686d26d518087
Anatolii Rotckin <rokkker@yandex.ru>
2023-02-07 11:51:12 +0200

    fix: исправить ошибки в верстке

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,22 +5,30 @@ function Portfolio({ portfolio }) {
    return (
       <article className="portfolio" id={portfolio}>
          <h3 className="portfolio__title">Портфолио</h3>
-         <nav className="portfolio__linkList">
-            <LinkPortfolio
-               link="https://rotckinan.github.io/how-to-learn/"
-               linkText="(статичный сайт)"
-               siteTitle="Научиться учиться"
-            />
-            <LinkPortfolio
-               link="https://rotckinan.github.io/russian-travel/"
-               linkText="(адаптивный сайт)"
-               siteTitle="Путешествия по России"
-            />
-            <LinkPortfolio
-               link="https://rotckinan.github.io/react-mesto-auth/"
-               linkText="(одностраничное приложение)"
-               siteTitle="Mesto Russia"
-            />
+         <nav>
+            <ul className="portfolio__linkList">
+               <li className="portfolio__link">
+                  <LinkPortfolio
+                     link="https://rotckinan.github.io/how-to-learn/"
+                     linkText="(статичный сайт)"
+                     siteTitle="Научиться учиться"
+                  />
+               </li>
+               <li className="portfolio__link">
+                  <LinkPortfolio
+                     link="https://rotckinan.github.io/russian-travel/"
+                     linkText="(адаптивный сайт)"
+                     siteTitle="Путешествия по России"
+                  />
+               </li>
+               <li className="portfolio__link">
+                  <LinkPortfolio
+                     link="https://rotckinan.github.io/react-mesto-auth/"
+                     linkText="(одностраничное приложение)"
+                     siteTitle="Mesto Russia"
+                  />
+               </li>
+            </ul>
          </nav>
       </article>
    );
